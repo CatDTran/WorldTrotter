3e78bc9de4895819d8ddccdb939fb0364ed08cf2
CarrotOnMacPro <trandinhcat@gmail.com>
2016-06-01 18:22:53 -0500

    playing with frame and view

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -12,7 +12,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let firstFrame = CGRect(x:160, y:240, width:100, height:150)// create Frame for a UIView
+        let firstView = UIView(frame: firstFrame)//create a UIView for the Frame instance above
+        firstView.backgroundColor = UIColor.blueColor()//set background color for this view as blue
+        view.addSubview(firstView)//add the new UIView to view
+        
+        let secondFrame = CGRect(x:20, y:30, width:50, height:50)
+        let secondView = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.greenColor()
+        //view.addSubview(secondView)
+        firstView.addSubview(secondView)
+        
     }
 
     override func didReceiveMemoryWarning() {
