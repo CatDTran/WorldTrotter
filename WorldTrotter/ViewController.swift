7139c5ed85d0f2d8db645f6ecff32b000f5f87db
Sriracha Sauce <sriracha-sauce@srirachas-mbp.attlocal.net>
2016-06-01 00:53:57 -0500

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Sriracha Sauce on 6/1/16.
+//  Copyright © 2016 Sriracha Sauce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
