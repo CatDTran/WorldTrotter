87f1c86ef8507f7e3ff0e3016c070ddeea237817
CarrotOnMacPro <trandinhcat@gmail.com>
2016-06-02 15:59:33 -0500

    Convert fahrenheit for celsius and update the celsiusLabel whenever top TextField changes

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -12,14 +12,38 @@ class ConversionViewController: UIViewController{
     
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
+    var fahrenheitValue: Double?{
+        didSet{
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double?{
+        if let value = fahrenheitValue{//check if there is fahrenheitValue, if there is, convert to celsius
+            return (value - 32) * (5/9)
+        }
+        else{//if not, return nil
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel(){
+        if let value = celsiusValue{
+            celsiusLabel.text = "\(value)"
+        }
+        else{
+            celsiusLabel.text = "???"
+        }
+    }
     
     @IBAction func fahrenheitFeildEditingChanged(textField: UITextField){
         //celsiusLabel.text = textField.text
         //if display ??? if celsiusLabel.text is empty
-        if let text = textField.text where !text.isEmpty{//if text field has text and it's not empty, it will set celsiusLabel
-            celsiusLabel.text = text
-        }else{
-            celsiusLabel.text = "???"
+        if let text = textField.text, value = Double(text){
+            fahrenheitValue = value
+        }
+        else{
+            fahrenheitValue = nil
         }
     }
     
