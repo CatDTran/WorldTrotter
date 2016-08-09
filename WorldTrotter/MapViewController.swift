afe32fb8e54b28f91ae2996f08a8685e8210383c
CarrotOnMacPro <trandinhcat@gmail.com>
2016-08-09 14:08:14 -0500

    Revert "Enable selecting different map types programmatically for the Segmented Control"
    This reverts commit d66ec966df5948792c24f1dbb673ecc9f158185e.
    
    # Conflicts:
    #	WorldTrotter/MapViewController.swift

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -18,16 +18,19 @@ class MapViewController: UIViewController{
         let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satelite"])    //create segment controll with 3 options
         segmentedControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)    //set background color and transparency
         segmentedControl.selectedSegmentIndex = 0   //initial selected option
+<<<<<<< HEAD
         segmentedControl.addTarget(self, action: #selector(MapViewController.mapTypeChanged(_:)), forControlEvents: .ValueChanged)
+=======
+>>>>>>> parent of d66ec96... Enable selecting different map types programmatically for the Segmented Control
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false  //turn this off because we are using Auto Layout
         view.addSubview(segmentedControl)
         
-        let topConstraint = segmentedControl.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 8)  //constraint the top anchor to top layout guide
+        let topConstraint = segmentedControl.topAnchor.constraintEqualToAnchor(topLayoutGuide.bottomAnchor, constant: 8)  //constraint the top anchor to root view's top anchor
         //let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor)  //constraint the leading anchor to the root view's leading anchor
         //let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor)   //constraint the trailing anchor to the root view's trailing anchor
-        let margins = view.layoutMarginsGuide   //get layout margins of this View Controller's view
-        let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)//constraint the leading edge to margin
-        let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)//constraint the trailing edge to margin
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraintEqualToAnchor(margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
         topConstraint.active = true //these constraints must be activated before they can have any effect
         leadingConstraint.active = true
         trailingConstraint.active = true
@@ -36,18 +39,5 @@ class MapViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Map View Controller loaded its view...")
-    }
-    
-    func mapTypeChanged(segControl: UISegmentedControl){
-        switch segControl.selectedSegmentIndex{
-        case 0:
-            mapView.mapType = .Standard
-        case 1:
-            mapView.mapType = .Hybrid
-        case 2:
-            mapView.mapType = .Satellite
-        default:
-            break
-        }
     }
 }
